f38a353b59ba4c348d36109b19f0736bb142db5f
Mariusq17 <ignatmarius45@gmail.com>
2025-03-24 10:32:51 +0200

    logout baietiii

--- cocodjango/userApp/urls.py
+++ cocodjango/userApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard/", views.controlPanel, name='controlPanel')
+    path("dashboard/", views.controlPanel, name='controlPanel'),
+    path("logout/", views.update_last_online, name='logoutUser'),
 ]
--- cocodjango/userApp/views.py
+++ cocodjango/userApp/views.py
@@ -1,12 +1,38 @@
-from lib2to3.fixes.fix_input import context
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 from .models import Employee
+import datetime
 def controlPanel(request):
     email = request.session.get('user_email')
+
+    if not email:
+        return redirect("home")
+
     name = Employee.objects.get(email=email).name
-    context = {"name": name}
+    user = Employee.objects.get(email=email)
+    if request.method == "POST":
+        user.last_time_online = datetime.datetime.now()
+        user.save()
+        return redirect("formPage")
+    context = {"name": name, 'user': user}
     return render(request, 'userApp/controlPanel.html', context)
+
+@csrf_exempt
+def update_last_online(request):
+    """Actualizează last_time_online la ieșirea utilizatorului"""
+    user_email = request.session.get('user_email')
+
+    if not user_email:  # Dacă sesiunea nu există, nu face nimic
+        return JsonResponse({"status": "session expired"}, status=403)
+
+    try:
+        user = Employee.objects.get(email=user_email)
+        user.last_time_online = datetime.datetime.now()
+        user.save()
+        return JsonResponse({"status": "success"})
+    except Employee.DoesNotExist:
+        return JsonResponse({"status": "error"}, status=404)
